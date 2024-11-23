c135f2f6eae0d5276f721c71f2a7c6f3dc7e2f8c
Parceva1 <stas202004@yandex.ru>
2024-11-24 00:20:07 +0300

    new domen

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,4 +1,5 @@
 import os
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.170.194', 'realnoeblindelo.ddns.net', 'www.realnoeblindelo.ddns.net']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '89.169.170.194', 'realnoeblindelo.bounceme.net', 'www.realnoeblindelo.bounceme.net']
 
 
 # Application definition
